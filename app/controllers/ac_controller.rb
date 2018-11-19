2d1fee9b21994bd4b7b644021f588ff95512aea1
bc-nogueira <bcnogueira@id.uff.br>
2018-11-19 20:58:25 -0200

    Completando actions de ac.

--- app/controllers/ac_controller.rb
+++ app/controllers/ac_controller.rb
@@ -1,4 +1,7 @@
 class AcController < ApplicationController
-  def index
-  end
+  def index; end
+
+  def new; end
+
+  def show; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   get 'ac/index'
+  get 'ac/new'
+  get 'ac/show'
+
   get 'inscricao/index'
   get 'inscricao/new'
   get 'inscricao/show'
