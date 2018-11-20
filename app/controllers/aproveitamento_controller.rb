d9dfe4e54eff12c0df159a208c5029c8a6efb499
bc-nogueira <bcnogueira@id.uff.br>
2018-11-19 23:59:50 -0200

    Adicionando funcionalidades de aproveitamento de disciplina.

---
+++ app/controllers/aproveitamento_controller.rb
@@ -0,0 +1,7 @@
+class AproveitamentoController < ApplicationController
+  def index; end
+
+  def new; end
+
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get 'ac/new'
   get 'ac/show'
 
+  get 'aproveitamento/index'
+  get 'aproveitamento/new'
+  get 'aproveitamento/show'
+
   get 'inscricao/index'
   get 'inscricao/new'
   get 'inscricao/show'
